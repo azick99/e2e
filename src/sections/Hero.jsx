bea1c2bbbdd0e719137bae8f21ad2f2b606db577
@azick99 <yunusaliyevazizbek80@gmail.com>
2023-10-27 15:57:18 +0200

    hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,12 +1,19 @@
+import whiteTech from '../assets/white-tech.jpg'
 import { motion } from 'framer-motion'
+
 const Hero = () => {
   return (
     <motion.section
       id="about"
-      className="pb-[100px] pt-[80px] flex justify-center relative ease-out scroll-mt-40 widescreen:section-min-height  tallscreen:section-min-height bg-[url('./white-tech.jpg')] bg-cover"
+      className="pb-[100px] pt-[80px] flex justify-center relative ease-out scroll-mt-40 widescreen:section-min-height  tallscreen:section-min-height "
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
     >
+      <img
+        src={whiteTech}
+        alt="white tech"
+        className="absolute w-full sm:h-[95%] h-full object-cover top-0 -z-10"
+      />
       <motion.article
         className="flex flex-col gap-5 md:w-[60vw] w-[90vw] text-center items-center ease-out"
         initial={{ scale: 1.3 }}
